36c32d339da2825fd723c2efb9d16c3cbf60e8fc
B-sirius <padfoot_07@163.com>
2017-05-11 18:49:55 +0800

    design?

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <div class="container">
+            <div class="sidebar"></div>
+            <div class="main">
+                <header class="header">
+                    <div class="title-container">
+                        
+                    </div>
+                </header>
+            </div>
+        </div>
+    </div>
 </template>
-
 <script>
 export default {
-  name: 'app'
+    name: 'app'
 }
 </script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Microsoft Yahei", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    .container {
+        display: flex;
+        height: 100vh;
+        .sidebar {
+            width: 60px;
+            background: linear-gradient(180deg, #7CCFDF, #80E3CE);
+        }
+        .main {
+            flex-grow: 1;
+            overflow: auto;
+            box-shadow: 0 0 10px #616161;
+            background: #F8F9FB;
+            padding: 20px 40px;
+            .header {
+                height: 120px;
+                border-bottom: 1px solid #ddd;
+                .title-container {
+                    display: flex;
+                    flex-direction: column;
+                }
+                .title {
+                    
+                }
+            }
+        }
+    }
 }
 </style>
