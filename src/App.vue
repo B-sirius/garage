02c609f940552908197ca7133782bc576b840522
B-sirius <padfoot_07@163.com>
2017-05-12 19:10:15 +0800

    v0.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
     <div id="app">
         <div class="container">
-            <div class="sidebar"></div>
-            <div class="main">
-                <header class="header">
-                    <div class="title-container">
-                        
+            <header class="header">
+                <h1 class="title">IT'S SHOW TIME <span class="face">∠( ᐛ 」∠)_ ...</span> </h1>
+                <div class="link-container">
+                    <ul class="list">
+                        <li class="list-item">
+                            <a class="blog-link" href="http://b-sirius.me/" target="_blank">SIRIUS'S GARAGE --></a>
+                        </li>
+                        <li class="list-item">
+                            <a class="blog-link" href="https://github.com/B-sirius" target="_blank">GITHUB --></a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+            <div class="content">
+                <div class="work-container">
+                    <div class="work-info">
+                        <div class="info-container">
+                            <span class="dark">MUSIC-PLAYER</span>
+                        </div>
                     </div>
-                </header>
+                    <a class="link" href="http://music.b-sirius.me/" target="_blank"></a>
+                </div>
             </div>
         </div>
     </div>
@@ -18,30 +33,93 @@ export default {
 }
 </script>
 <style lang="scss">
+$haxagonURL: "http://7xrkxs.com1.z0.glb.clouddn.com/garage/hexagon.svg";
 #app {
-    font-family: "Microsoft Yahei", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    font-family: "Delius Unicase", cursive, "Microsoft Yahei", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+    background: #26252A;
     .container {
-        display: flex;
-        height: 100vh;
-        .sidebar {
-            width: 60px;
-            background: linear-gradient(180deg, #7CCFDF, #80E3CE);
-        }
-        .main {
-            flex-grow: 1;
-            overflow: auto;
-            box-shadow: 0 0 10px #616161;
-            background: #F8F9FB;
-            padding: 20px 40px;
-            .header {
-                height: 120px;
-                border-bottom: 1px solid #ddd;
-                .title-container {
+        position: relative;
+        max-width: 1200px;
+        min-height: 100vh;
+        margin: auto;
+        padding: 40px 20px;
+        .header {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 50px;
+            .link-container {
+                display: flex;
+                .list {
+                    margin: auto;
                     display: flex;
                     flex-direction: column;
+                    align-items: flex-end;
+                    .list-item {
+                        margin-bottom: 5px;
+                        &:last-child {
+                            margin: 0;
+                        }
+                        .blog-link {
+                            color: #fff;
+                            text-decoration: none;
+                            font-size: 20px;
+                        }
+                    }
                 }
-                .title {
-                    
+            }
+            .title {
+                color: #fff;
+                font-size: 64px;
+                .face {
+                    font-size: 20px;
+                    vertical-align: 4px;
+                }
+            }
+        }
+        .content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            .work-container {
+                position: relative;
+                width: 180px;
+                height: 300px;
+                background: #828282;
+                margin-bottom: 60px;
+                margin-right: 50px;
+                border-radius: 15px;
+                .work-info {
+                    display: flex;
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    background: url($haxagonURL) center no-repeat;
+                    background-size: 140px 140px;
+                    transform: translate3d(0, 0, 0);
+                    transition: 0.2s;
+                    .info-container {
+                        display: flex;
+                        flex-direction: column;
+                        margin: auto;
+                        .dark, .light {
+                            font-family: 'BenchNine', sans-serif;
+                            font-size: 22px;
+                        }
+                        .dark {
+                            color: #414042;
+                        }
+                        .light {
+                            color: #b1b3b6;
+                        }
+                    }
+                }
+                &:hover .work-info {
+                    transform: rotateY(90deg);
+                }
+                .link {
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
                 }
             }
         }
